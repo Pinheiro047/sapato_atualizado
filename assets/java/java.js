09cd355bfad31d73d57d07152adf32a1a67469aa
Felipe <Pinheirofelipe741@gmail.com>
2023-05-15 08:58:26 -0300

    fazendo o java script

---
+++ assets/java/java.js
@@ -0,0 +1,8 @@
+let btn_btn = document.querySelector('.btn_btn')
+
+let navbar = document.querySelector('.navbar')
+
+
+btn_btn.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbar_ativo')
+})
